e2cb4e425218a62c01fb94195173d76e188b1eaf
zhonghao.wang <wzhh1994@gmail.com>
2021-10-20 16:44:45 +0800

    +x for nvm

--- install.sh
+++ install.sh
@@ -103,4 +103,5 @@ cat $git_home/.zsh_export >> ~/.zshrc
 
 curl -o- https://cdn.jsdelivr.net/gh/nvm-sh/nvm@v0.39.0/install.sh  | bash
 NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+chmod +x $NVM_DIR/nvm.sh
 $NVM_DIR/nvm.sh install node
